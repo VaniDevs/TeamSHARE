1aaeebf732cdc4ec52328741a61a44c2817e56f6
Radha Satam <radha.satam@gmail.com>
2018-09-23 01:28:55 -0700

    fix issues with event data

--- src/data/events.js
+++ src/data/events.js
@@ -13,8 +13,8 @@ export default [
     "lastName": "Simeoli",
     "phoneNumber": "(728) 3770242",
     "email": "msimeoli0@wufoo.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
     "id": "78-927-4768",
@@ -23,8 +23,8 @@ export default [
     "lastName": "Eddowis",
     "phoneNumber": "(748) 1595966",
     "email": "ceddowis1@facebook.com",
-    "start": new Date(2018, 08, 4, 12, 00, 00),
-    "end": new Date(2018, 08, 4, 12, 15, 00)
+    "start": new Date(2018, 8, 4, 12, 0, 0),
+    "end": new Date(2018, 8, 4, 12, 15, 0)
   },
   {
     "id": "37-341-0425",
@@ -33,18 +33,18 @@ export default [
     "lastName": "Winspire",
     "phoneNumber": "(456) 6565191",
     "email": "rwinspire2@adobe.com",
-    "start": new Date(2018, 08, 11, 13, 30, 00),
-    "end": new Date(2018, 08, 11, 13, 45, 00)
+    "start": new Date(2018, 8, 11, 13, 30, 0),
+    "end": new Date(2018, 8, 11, 13, 45, 0)
   },
   {
     "id": "40-625-9202",
-    "title": "Quercus \u00d7incomita Palmer",
+    "title": "Quercus incomita Palmer",
     "firstName": "Anatola",
     "lastName": "Doley",
     "phoneNumber": "(537) 9381787",
     "email": "adoley3@wix.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
     "id": "71-825-6763",
@@ -53,18 +53,18 @@ export default [
     "lastName": "Heintz",
     "phoneNumber": "(856) 4863986",
     "email": "jheintz4@theglobeandmail.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
-    "id": "74-017-0956",
+    "id": "74-017-956",
     "title": "Eurybia glauca (Nutt.) G.L. Nesom var. glauca",
     "firstName": "Elinor",
     "lastName": "Alpe",
     "phoneNumber": "(239) 6771784",
     "email": "ealpe5@reuters.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
     "id": "33-632-2868",
@@ -73,18 +73,18 @@ export default [
     "lastName": "Fossitt",
     "phoneNumber": "(897) 8855510",
     "email": "jfossitt6@yelp.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
-    "id": "56-300-7451",
+    "id": "56-30-7451",
     "title": "Lobelia yuccoides Hillebr.",
     "firstName": "Obediah",
     "lastName": "Bennison",
-    "phoneNumber": "(630) 7350947",
+    "phoneNumber": "(630) 735947",
     "email": "obennison7@umich.edu",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
     "id": "38-269-2706",
@@ -93,8 +93,8 @@ export default [
     "lastName": "Bowland",
     "phoneNumber": "(533) 1016240",
     "email": "ebowland8@chicagotribune.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   },
   {
     "id": "93-650-3717",
@@ -103,8 +103,8 @@ export default [
     "lastName": "Tejero",
     "phoneNumber": "(523) 2833148",
     "email": "ttejero9@un.org",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45, 0),
+    "end": new Date(2018, 8, 4, 10, 15, 0)
   }
 ]
 
