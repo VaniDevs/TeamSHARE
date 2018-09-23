44a2c107a7cde1d93507685170d48af80d304513
Radha Satam <radha.satam@gmail.com>
2018-09-23 00:10:55 -0700

    fetch agency profile action

--- src/components/Pages/Dashboard/AgencyEmpDashboard.js
+++ src/components/Pages/Dashboard/AgencyEmpDashboard.js
@@ -4,7 +4,13 @@ class AgencyEmpDashboard extends Component {
 
     render() {
         return (
-            <div>Agency Emp Dashboard</div>
+            <div>
+                <h1>Dashboard</h1>
+                <p>Calendar</p>
+                <p>Include Stats for Number of Clients</p>
+                <p>A big button to add a new client</p>
+                <p>Spread the Word. Tweet / Blah / Social</p>
+            </div>
         )
     }
 }
