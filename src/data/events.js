0a4a4408c978de9b6b2d9212fd7727e5c8065474
Eina <einers@outlook.com>
2018-09-23 02:32:40 -0700

    fix events

--- src/data/events.js
+++ src/data/events.js
@@ -13,8 +13,8 @@ export default [
     "lastName": "Simeoli",
     "phoneNumber": "(728) 3770242",
     "email": "msimeoli0@wufoo.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 4, 9, 45),
+    "end": new Date(2018, 8, 4, 10, 15)
   },
   {
     "id": "78-927-4768",
@@ -23,8 +23,8 @@ export default [
     "lastName": "Eddowis",
     "phoneNumber": "(748) 1595966",
     "email": "ceddowis1@facebook.com",
-    "start": new Date(2018, 08, 4, 12, 00, 00),
-    "end": new Date(2018, 08, 4, 12, 15, 00)
+    "start": new Date(2018, 8, 4, 12),
+    "end": new Date(2018, 8, 4, 12, 15)
   },
   {
     "id": "37-341-0425",
@@ -33,8 +33,8 @@ export default [
     "lastName": "Winspire",
     "phoneNumber": "(456) 6565191",
     "email": "rwinspire2@adobe.com",
-    "start": new Date(2018, 08, 11, 13, 30, 00),
-    "end": new Date(2018, 08, 11, 13, 45, 00)
+    "start": new Date(2018, 8, 11, 13, 30),
+    "end": new Date(2018, 8, 11, 13, 45)
   },
   {
     "id": "40-625-9202",
@@ -43,8 +43,8 @@ export default [
     "lastName": "Doley",
     "phoneNumber": "(537) 9381787",
     "email": "adoley3@wix.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 14, 11, 30),
+    "end": new Date(2018, 8, 14, 11, 45)
   },
   {
     "id": "71-825-6763",
@@ -53,18 +53,18 @@ export default [
     "lastName": "Heintz",
     "phoneNumber": "(856) 4863986",
     "email": "jheintz4@theglobeandmail.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 25, 9, 45),
+    "end": new Date(2018, 8, 25, 10, 15)
   },
   {
-    "id": "74-017-0956",
+    "id": "74-017-956",
     "title": "Eurybia glauca (Nutt.) G.L. Nesom var. glauca",
     "firstName": "Elinor",
     "lastName": "Alpe",
     "phoneNumber": "(239) 6771784",
     "email": "ealpe5@reuters.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 27, 4, 14, 45),
+    "end": new Date(2018, 27, 4, 14, 15)
   },
   {
     "id": "33-632-2868",
@@ -73,18 +73,18 @@ export default [
     "lastName": "Fossitt",
     "phoneNumber": "(897) 8855510",
     "email": "jfossitt6@yelp.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 18, 12),
+    "end": new Date(2018, 8, 18, 12, 30)
   },
   {
     "id": "56-300-7451",
     "title": "Lobelia yuccoides Hillebr.",
     "firstName": "Obediah",
     "lastName": "Bennison",
-    "phoneNumber": "(630) 7350947",
+    "phoneNumber": "(630) 735947",
     "email": "obennison7@umich.edu",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 8, 20, 9, 45),
+    "end": new Date(2018, 8, 20, 10, 15)
   },
   {
     "id": "38-269-2706",
@@ -93,8 +93,8 @@ export default [
     "lastName": "Bowland",
     "phoneNumber": "(533) 1016240",
     "email": "ebowland8@chicagotribune.com",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 9, 2, 9, 45),
+    "end": new Date(2018, 9, 2, 10, 15)
   },
   {
     "id": "93-650-3717",
@@ -103,8 +103,8 @@ export default [
     "lastName": "Tejero",
     "phoneNumber": "(523) 2833148",
     "email": "ttejero9@un.org",
-    "start": new Date(2018, 08, 4, 09, 45, 00),
-    "end": new Date(2018, 08, 4, 10, 15, 00)
+    "start": new Date(2018, 9, 5, 9, 45),
+    "end": new Date(2018, 9, 5, 10, 15)
   }
 ]
 
