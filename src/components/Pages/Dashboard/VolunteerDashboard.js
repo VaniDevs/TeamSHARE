8cdcaa197c5c75c767942179e04c37f8ede10d16
Radha Satam <radha.satam@gmail.com>
2018-09-23 02:54:16 -0700

    update dashboard

--- src/components/Pages/Dashboard/VolunteerDashboard.js
+++ src/components/Pages/Dashboard/VolunteerDashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import CountUp from 'react-countup';
-
+import Graph from './Graph';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import events from '../../../data/events'
@@ -13,59 +13,105 @@ class VolunteerDashboard extends Component {
     render() {
         return (
         <div className="b-page">
-          <h1 className="b-page__header">Volunteer Overview</h1>
+          <h1 className="b-page__header">Overview</h1>
 
-            <div className="o-panel-container">
-                <div>
-                    <h4>Inventory Stock</h4>
+            
+                <div class="o-grid o-grid--demo">
+                <div class="o-grid__cell">
+                    <div class="o-grid-text">
+                        <div class="c-card">
+                            <div class="c-card__body">
+                                <h4>
+                                <CountUp 
+                                    start={0}
+                                    end={122}
+                                />
+                                &nbsp;Families</h4>
+                                <p>Helped This Month</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="o-grid__cell">
+                    <div class="o-grid-text">
+                    <div class="c-card">
+                        <div class="c-card__body">
+                            <h4>
+                                <CountUp 
+                                    start={0}
+                                    end={432}
+                                />&nbsp;Items</h4>
+                            <p>Donated This Month</p>
+                        </div>
+                    </div>
+                     
+                    </div>
+                </div>
+                <div class="o-grid__cell">
+                    <div class="o-grid-text">
+                    <div class="c-card">
+                        <div class="c-card__body">
+                            <h4>
+                                <CountUp 
+                                    start={0}
+                                    end={444}
+                                />&nbsp;
+                                Hours</h4>
+                            <p>Volunteered This Month</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
 
-                </div>
-                <div>
-                    <h4>Updates For Volunteers</h4>
-                    <p>As per new government rules, it's mandatory to do ...</p>
-                </div>
-                <div>
-                    <h4>
-                        <CountUp 
-                            start={0}
-                            end={122}
-                        />
-                        Families</h4>
-                    <p>Helped This Month</p>
-                </div>
-                <div>
-                    <h4>
-                        <CountUp 
-                            start={0}
-                            end={432}
-                        />
-                    Items</h4>
-                    <p>Donated This Month</p>
-                </div>
-                 <div>
-                    <h4>
-                        <CountUp 
-                            start={0}
-                            end={444}
-                        />
-                        Hours</h4>
-                    <p>Volunteered This Month</p>
-                </div>
 
-                <div>
-                    <h4>Calendar</h4>
-                    <div style={{ height: 400 }}>
-                        <BigCalendar
-                            localizer={localizer}
-                            events={events}
-                            startAccessor="start"
-                            endAccessor="end"
-                        />
+                <div class="o-grid o-grid--demo">
+                    <div class="o-grid__cell">
+                        <div class="o-grid-text">
+                        <div class="c-card">
+                            <div class="c-card__body">
+                                <h4>Appointments Calendar</h4>
+                                    <div style={{ height: 400 }}>
+                                        <BigCalendar
+                                            localizer={localizer}
+                                            events={events}
+                                            startAccessor="start"
+                                            endAccessor="end"
+                                        />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
+                <div class="o-grid__cell">
+                    <div class="o-grid-text">
+                    <div class="c-card">
+                    <div role="separator" class="c-card__item c-card__item--divider">Inventory Stock</div>
+                        <div class="c-card__item  ">
+                            <p class="c-paragraph">Lorem ipsum dolor sit amet, feugiat corpora ex eam. Inciderint eloquentiam sea et.</p>
+                            <p class="c-paragraph">Lorem ipsum dolor sit amet, feugiat corpora ex eam. Inciderint eloquentiam sea et.</p>
+                        </div>
+                    </div>
+                    <div class="c-card">
+                    <div role="separator" class="c-card__item c-card__item--divider">Volunteer Updates</div>
+                        <div class="c-card__item  ">
+                            <p>As per new government rules, it's mandatory to do ...</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="c-card">
+                        <div role="separator" class="c-card__item c-card__item--divider">Inventory Stock</div>
+                        <div class="c-card__body">
+                            <Graph />
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+                
                 <p>Some kind of Bar Chart of Personal Chart?</p>
-            </div>
         </div>
         )
     }
