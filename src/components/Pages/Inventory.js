f17b167153705cbc904a0c4d19d9be4515b5454a
Radha Satam <radha.satam@gmail.com>
2018-09-23 00:34:07 -0700

    add notes about what goes on dashboard

--- src/components/Pages/Inventory.js
+++ src/components/Pages/Inventory.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import Griddle, { plugins, RowDefinition, ColumnDefinition } from 'griddle-react';
+import { Filter, GriddleLayout, PageSizeDropDownPlugin } from '../../utils/griddle';
 
+import { inventory } from "../../data/staticTableData";
 class Inventory extends Component {
+    constructor(props){
+      super(props)
+    
+      this.state = {
+          pageSize: 10
+      }
+
+      this._handlePageSizeChange = this._handlePageSizeChange.bind(this)
+    
+    }
+
+    _handlePageSizeChange(size) {
+        this.setState({ pageSize: size })
+    }
+    
 
     render() {
+        const data = inventory;
         return (
             <div>
-                Inventory
+                <Griddle
+                    data={data}
+                    plugins={[plugins.LocalPlugin, PageSizeDropDownPlugin({ pageSizes: [10, 20, 30] }, this._handlePageSizeChange)]}
+                    settingsComponentObjects={{ columnChooser: null }}
+                    components={{ Filter, Layout: GriddleLayout }}
+                    styleConfig={{
+                        classNames: {
+                            Table: "griddle-table b0 table table-striped table-hover dataTable  ",
+                            PageDropdown: 'griddle-page-select form-control',
+                            NextButton: "griddle-next-button btn",
+                            Pagination: "griddle-pagination pull-right",
+                            PreviousButton: "griddle-previous-button btn",
+                        }
+                    }}
+                / >
+                {/* <Griddle
+                    data={data}
+                    plugins={[plugins.LocalPlugin, PageSizeDropDownPlugin({ pageSizes: [10, 20, 30] }, this._handlePageSizeChange)]}
+                    settingsComponentObjects={{ columnChooser: null }}
+                    components={{ Filter, Layout: GriddleLayout }}
+                    styleConfig={{
+                        classNames: {
+                            Table: "griddle-table b0 table table-striped table-hover dataTable  ",
+                            PageDropdown: 'griddle-page-select form-control',
+                            NextButton: "griddle-next-button btn",
+                            Pagination: "griddle-pagination pull-right",
+                            PreviousButton: "griddle-previous-button btn",
+                        }
+                    }}
+                >
+                    <RowDefinition>
+                        <ColumnDefinition id="id" metadata={true} />
+                        <ColumnDefinition id="name" title="Name" />
+                        <ColumnDefinition id="phone" title="Phone" />
+                    </RowDefinition>
+                </Griddle> */}
             </div>
         )
     }
