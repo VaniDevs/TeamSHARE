b5ffaff5352f4c5a604e7ea9e21ffc14fb1f04ca
Radha Satam <radha.satam@gmail.com>
2018-09-23 01:26:23 -0700

    add countup

--- src/components/Pages/Dashboard/VolunteerDashboard.js
+++ src/components/Pages/Dashboard/VolunteerDashboard.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import CountUp from 'react-countup';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import events from '../../../data/events'
+const localizer = BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
 class VolunteerDashboard extends Component {
 
@@ -8,14 +16,55 @@ class VolunteerDashboard extends Component {
             <p>Volunteer Dashboard</p>
             <h1 className="b-page__header">Overview</h1>
             <div className="o-panel-container">
-                <p>Appointment Schdule Calendar</p>
-                <p>Inventory Stock</p>
-                <p>Message For Volunteers</p>
-                <p>Stats - 123 Families Helped this month</p>
-                <p>Stats - 46 Items Donated</p>
-                <p>Stats - 33 Volunteer Hours</p>
-                <p>Some kind of Bar Chart of Personal Chart?</p>
+                <div>
+                    <h4>Inventory Stock</h4>
 
+                </div>
+                <div>
+                    <h4>Updates For Volunteers</h4>
+                    <p>As per new government rules, it's mandatory to do ...</p>
+                </div>
+                <div>
+                    <h4>
+                        <CountUp 
+                            start={0}
+                            end={122}
+                        />
+                        Families</h4>
+                    <p>Helped This Month</p>
+                </div>
+                <div>
+                    <h4>
+                        <CountUp 
+                            start={0}
+                            end={432}
+                        />
+                    Items</h4>
+                    <p>Donated This Month</p>
+                </div>
+                 <div>
+                    <h4>
+                        <CountUp 
+                            start={0}
+                            end={444}
+                        />
+                        Hours</h4>
+                    <p>Volunteered This Month</p>
+                </div>
+
+                <div>
+                    <h4>Calendar</h4>
+                    <div style={{ height: 400 }}>
+                        <BigCalendar
+                            localizer={localizer}
+                            events={events}
+                            startAccessor="start"
+                            endAccessor="end"
+                        />
+                    </div>
+                </div>
+
+                <p>Some kind of Bar Chart of Personal Chart?</p>
             </div>
         </div>   
         )
